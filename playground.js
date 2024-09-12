800e57ef58ee794293162d4a8051f21b38fcd030
hungting li <hungtington.dev@gmail.com>
2024-09-12 13:48:18 +0800

    chore: make playground.js as an emulate env to quick test built result under node env

--- playground.js
+++ playground.js
@@ -1,3 +1,7 @@
-// @ts-nocheck // HACK:
+/**
+ * The goal of this file is trying to test the built result very quick from node env.
+ * The workspace setting should already let it work out of Deno env,
+ * but just in case, this file shouldn't be triggered via Deno in any way.
+ */
 const U = require('./npm/script/index.js');
 console.log(U.is('object', {}))
