093d60b24f509749db7929a73b17b4ae2d636ba9
timlee <hungtington.dev@gmail.com>
2024-10-21 06:30:47 +0800

    ✨ feat: support `falsy` type in `is` operator

--- src/is.ts
+++ src/is.ts
@@ -1,6 +1,11 @@
 import * as zod from "zod";
 
-type Types = 'object' | 'boolean' | 'array' | 'string' | 'function';
+type Types = 'object' | 'boolean' | 'array' | 'string' | 'function' | 'falsy';
+
+// Note: This type includes `number` which covers `NaN`, but also includes all other numbers.
+// The runtime check in isFalsy() handles NaN correctly.
+// (ie., TS doesn't have a specific type for `NaN`)
+type IsFalsyReturnType = null | undefined | '' | [] | {} | false | 0 | number;
 
 type ExtractType<T extends Types> =
   T extends 'object' ? object :
@@ -8,11 +13,14 @@ type ExtractType<T extends Types> =
   T extends 'array' ? any[] :
   T extends 'string' ? string :   // deno-lint-ignore no-explicit-any
   T extends 'function' ? (...args: any[]) => any :
+  T extends 'falsy' ? IsFalsyReturnType :
   never;
 
 export const is = <T extends Types>(type: T, value: unknown): value is ExtractType<T> =>
 {
-  const shapeMap: Record<Types, unknown> = {
+  if (type === 'falsy') return isFalsy(value)
+
+  const shapeMap: Record<Exclude<Types, 'falsy'>, unknown> = {
     object: {},
     boolean: undefined,
     array: zod.any(), // usage: `its('array', value)`
@@ -20,6 +28,17 @@ export const is = <T extends Types>(type: T, value: unknown): value is ExtractTy
     function: zod.function()
   };
   // deno-lint-ignore no-explicit-any
-  const method = zod[type] as (...args: any[]) => any;
-  return method(shapeMap[type]).safeParse(value).success;
+  const method = zod[type as Exclude<T, 'falsy'>] as (...args: any[]) => any;
+  return method(shapeMap[type as Exclude<T, 'falsy'>]).safeParse(value).success;
 }
+
+const isFalsy = ($v: any): $v is IsFalsyReturnType =>
+{
+  if (typeof $v === 'number' && ($v <= 0 || Number.isNaN($v))) return true;
+  const someFalsies = ['', false, null, undefined];
+  return (
+    someFalsies.includes($v as any) ||
+    (Array.isArray($v) && $v.length === 0) ||
+    (typeof $v === 'object' && $v !== null && Object.keys($v).length === 0)
+  );
+};
